061f87e92641af990992022255185c5ec969d804
Christina Koller <ckoller@panoramaed.com>
2025-09-30 15:29:39 -0400

    Update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://bostonmesh.github.io/',
+  url: 'https://bostonme.sh/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/bostonmesh/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
